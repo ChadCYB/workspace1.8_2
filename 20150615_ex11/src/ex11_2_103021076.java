9b5e3a60c2475b2da4a9216049519f1d766b3300
Yi-Cheng Chen <Chad.CYB@gmail.com>
2015-06-21 15:18:33 +0800

    ex11 fix

--- 20150615_ex11/src/ex11_2_103021076.java
+++ 20150615_ex11/src/ex11_2_103021076.java
@@ -1,33 +1,53 @@
 
 public class ex11_2_103021076 {
+	static int count = 0;
 	public static void main(String[] avg){
-		int[][] img = randImg();
-		System.out.println(TransCode(img,0,img.length,0,img.length,true,""));
+		int len = 8;
+		int[][] img = randImg(len);
+		boolean tt = false;
+		String str = (TransCode(img,0,0,len/2,len,!tt))
+				   + (TransCode(img,len/2,0,len,len,!tt));	//第一次直切
+		System.out.println(str);
+		System.out.println(toDec(str)+":"+str.length());
 	}
-	public static String TransCode(int[][] data, int x1, int y1, int x2, int y2, boolean tt,String str){
-		int len = data.length;
-		for(int i=x1 ; i<x2 ; i++){
-			for(int j=y1 ; j<y2 ; j++){
+
+	public static String TransCode(int[][] data, int x1, int y1, int x2, int y2, boolean tt){
+		System.out.println("<"+(count++)+">("+x1+","+y1+" | "+x2+","+y2+"):"+tt);
+		String str = "";
+		try{
+			str = check1(data, x1, y1, x2, y2);
+			if(Math.abs(x1-x2)<=1 && Math.abs(y1-y2)<=1 ||(x1==x2 && x2==y1 && y2==1)){ //終止條件 x1 = x2
+				return str;
+			}else{
+				if(tt){		//直切
+					int xHalf = ((x1+x2)/2 == x1) ? x2:(x1+x2)/2;
+					return str + (TransCode(data,x1,y1,xHalf,y2,!tt))
+							   + (TransCode(data,xHalf,y1,x2,y2,!tt));
+				}else{		//橫切
+					int yHalf = ((y1+y2)/2 == y1) ? y2:(y1+y2)/2;
+					return str + (TransCode(data,x1,y1,x2,yHalf,!tt))
+							   + (TransCode(data,x1,yHalf,x2,y2,!tt));
+				}
+			}
+		}catch(StackOverflowError ee){
+			ee.printStackTrace();
+			return str;
+		}
+	}
+	public static String check1(int[][] data,int x1, int y1, int x2, int y2){
+		String str = "";
+		for(int i=y1 ; i<y2 ; i++){
+			for(int j=x1 ; j<x2 ; j++){
 				if(data[i][j] > 0){
-					return "1"+;
+					str = "1";
 				}else{
-					return "0"+;
+					str = "0";
 				}
 			}
 		}
-	//終止條件 x1 = x2
-		if(tt){		//直切
-			str += 
-		}else{		//橫切
-			 
-		}
-		str = (TransCode(data,0,len/2)) + (TransCode(data,len/2,len));
 		return str;
 	}
-	
-	
-	public static int[][] randImg(){
-		int len = 8;
+	public static int[][] randImg(int len){
 		int[][] arr = new int[len][len];
 		for(int i=0 ; i<len ; i++){
 			for(int j=0 ; j<len ; j++){
@@ -41,5 +61,12 @@ public class ex11_2_103021076 {
 			System.out.println();
 		}	
 		return arr;
+	}
+	public static int toDec(String dec){
+		int sum = 0;
+		for(int i=0 ; i<dec.length() ; i++){
+			sum += Integer.parseInt(dec.substring(i,i+1))*Math.pow(2, i);
+		}
+		return sum;
 	}
 }
